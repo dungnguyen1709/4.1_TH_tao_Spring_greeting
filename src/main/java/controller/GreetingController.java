811c670ec07973effa3db08ddb166c6de60cc556
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-09-14 00:43:57 +0700

    commit

--- src/main/java/controller/GreetingController.java
+++ src/main/java/controller/GreetingController.java
@@ -5,11 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+//@Controller
+//public class GreetingController {
+//    @GetMapping("/greeting")
+//    public String greeting(@RequestParam String name, Model modle){
+//        modle.addAttribute("name", name);
+//        return "index";
+//    }
+//}
+
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
-    public String greeting(@RequestParam String name, Model modle){
-        modle.addAttribute("name", name);
+    public String greeting(){
         return "index";
     }
 }
